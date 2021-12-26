64c5d8f453758ea17811af03d2a6bd66b927cb48
asmaafayed02 <asmaafayed02@gmail.com>
2021-12-26 15:57:43 +0200

    rent website

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var aside = document.getElementById('rental')
+var asidecont = aside.innerHTML
+console.log(asidecont);
+var canvasBody = document.querySelector('.offcanvas-body')
+canvasBody.innerHTML = asidecont
